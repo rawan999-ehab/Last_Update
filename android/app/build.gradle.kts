cdfb5b5488fc64c9e05a0dd23ac93635239e5ae6
rawan999-ehab <rawan_e16048@cic-cairo.com>
2025-03-28 23:16:48 +0200

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.fg_company_after_database"
+    compileSdk = flutter.compileSdkVersion
+
+    // Set the correct NDK version required by your plugins
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.fg_company_after_database"
+        // Update minSdkVersion to 23 to fix the compatibility issue
+        minSdk = 23 // Set to 23 as required by firebase_auth plugin
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/fg_company_after_database/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fg_company_after_database
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
